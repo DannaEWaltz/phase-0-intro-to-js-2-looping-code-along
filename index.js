895e1494b9e14331d640baecce81c4b7b94c59af
+ <+dannaewaltz@gmail.com>
2022-06-19 20:00:35 -0600

    commit tree 1959240b5086cf4922e7742a9d12390591d11d4c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+namesArray = ["Guadalupe", "Ollie", "Aki"];
+beforeEach( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+
+  afterEach( () => {
+    spy.restore();
+  } );
+function writeCards(namesArray, surprise) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
+
